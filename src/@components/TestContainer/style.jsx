c124558abcd5ad41fc90334de25d07ea2971264d
junzzi <jjh01027717502@gmail.com>
2022-07-29 20:54:20 +0900

    style: TestContainer Style 추가

---
+++ src/@components/TestContainer/style.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const Root = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Button = styled.button`
+  padding: 10px 50px;
+
+  border: none;
+  border-radius: 20px;
+
+  background-color: #ff2970;
+  color: #ffffff;
+
+  cursor: pointer;
+`;
